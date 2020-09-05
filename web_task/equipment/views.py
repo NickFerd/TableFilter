d4989c7ea7e4ea6a5f876258570aba24a2b3d09b
Nick Ferdman <ferdman.inc@gmail.com>
2020-09-05 22:23:41 +0300

    - подключил bootstrap4 - настроил вывод таблицы - подключил пагинацию (40 строк таблицы на каждой странице)

--- web_task/equipment/views.py
+++ web_task/equipment/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from django.core.paginator import Paginator
+
+from .models import Duration
+
+
+def index(request):
+    query = Duration.objects.all().order_by('start')
+    paginator = Paginator(query, 40)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {'page_obj': page_obj}
+    return render(request, 'equipment/index.html', context)
+
+
+
--- web_task/web_task/settings.py
+++ web_task/web_task/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'equipment.apps.EquipmentConfig'
+    'equipment.apps.EquipmentConfig',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Settings for django-bootstrap4
+BOOTSTRAP4 = {
+    'include_jquery': True,
+}
