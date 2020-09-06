c034a9cf229bf875b1f784ba296c6056e2f1d82a
Nick Ferdman <ferdman.inc@gmail.com>
2020-09-06 20:31:49 +0300

    1) Починил пагинацию при установленных фильтрах (костыль) 2) добавил фильтрацию по клиентам, оборудованию, режиму и длительности

--- web_task/equipment/forms.py
+++ web_task/equipment/forms.py
@@ -1,6 +1,34 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from .models import Client, Mode, Equipment
+
+
+def get_list(model):
+    objects = model.objects.all()
+    obj_list = []
+    for obj in objects:
+        obj_list.append((obj.id, obj.name))
+    obj_list.append((-100, 'All'))
+    return obj_list
+
+
+def validate_positive(value):
+    if value < 0:
+        raise ValidationError(
+            'Input value should be positive',
+            params={'value': value}
+        )
 
 
 class DurationFilterForm(forms.Form):
-    start_date = forms.CharField(label='Начало (YYYY-MM-DD)', required=False)
-    stop_date = forms.CharField(label='Конец (YYYY-MM-DD)', required=False)
+    client = forms.MultipleChoiceField(choices=get_list(Client), label='Клиенты',
+                                       required=False)
+    mode = forms.MultipleChoiceField(choices=get_list(Mode), label='Режим работы',
+                                     required=False)
+    equip = forms.MultipleChoiceField(choices=get_list(Equipment), label='Оборудование',
+                                      required=False)
+    duration = forms.IntegerField(label='Длительность состояния', required=False,
+                                  validators=[validate_positive])
+    start_date = forms.DateField(label='Начало (YYYY-MM-DD)', required=False)
+    stop_date = forms.DateField(label='Конец (YYYY-MM-DD)', required=False)
+
--- web_task/equipment/views.py
+++ web_task/equipment/views.py
@@ -6,19 +6,45 @@ from .models import Duration
 from .forms import DurationFilterForm
 
 
-def index(request):
-    query = Duration.objects.all().order_by('start')
-    form = DurationFilterForm(request.GET)
-    if form.is_valid():
-        if form.cleaned_data['start_date']:
-            query = query.filter(start=form.cleaned_data['start_date'])
-
-    paginator = Paginator(query, 40)
-
+def paginate(queryset, request):
+    paginator = Paginator(queryset, 20)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
+    return page_obj
 
-    context = {'page_obj': page_obj, 'form': form}
+
+def index(request):
+    if request.method == 'POST':
+        form = DurationFilterForm()
+        query = Duration.objects.all().order_by('start')
+        link_querystring = ''
+    else:
+        query = Duration.objects.all().order_by('start')
+        form = DurationFilterForm(request.GET)
+        if form.is_valid():
+            # Filter query according to params
+            if form.cleaned_data['client'] and form.cleaned_data['client'] != -100:
+                query = query.filter(client_id__in=form.cleaned_data['client'])
+
+            if form.cleaned_data['mode'] and form.cleaned_data['mode'] != -100:
+                query = query.filter(mode_id__in=form.cleaned_data['mode'])
+
+            if form.cleaned_data['equip'] and form.cleaned_data['equip'] != -100:
+                query = query.filter(equipment_id__in=form.cleaned_data['equip'])
+
+            # time management
+            if form.cleaned_data['start_date']:
+                query = query.filter(start=form.cleaned_data['start_date'])
+
+            q = request.GET.copy()
+            try:
+                q.pop('page')
+            except KeyError:
+                pass
+            link_querystring = q.urlencode()
+
+    page_obj = paginate(query, request)
+    context = {'form': form, 'page_obj': page_obj, 'link_querystring': link_querystring}
     return render(request, 'equipment/index.html', context)
 
 
